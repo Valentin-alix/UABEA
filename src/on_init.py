dabc29db436d644cc0a26ee8acabeeebdd9a5c0a
Valentin Alix <valentin-alix.850@outlook.fr>
2024-10-31 12:34:22 +0100

    better python generator

--- src/on_init.py
+++ src/on_init.py
@@ -2,28 +2,34 @@ import os
 import sys
 from pathlib import Path
 
+from src.generator.generate_python_from_datas import gen_all_python_class_datas
+from src.on_maj import on_maj
 
 sys.path.append(str(Path(__file__).parent.parent))
 
 from db_dofus_unity.consts import (
     DOFUS_PATH,
 )
-from src.on_maj import on_maj
-from src.generator.generate_python_from_datas import gen_all_python_class_datas
+
+UABEA_PATH_EXE = os.path.join(
+    "D:\\",
+    "Workspace",
+    "UABEA",
+    "UABEAvalonia",
+    "bin",
+    "Debug",
+    "net6.0",
+    "UABEAvalonia.exe",
+)
+
+
+def get_gfx():
+    os.system(
+        f"{UABEA_PATH_EXE} batchexportbundle {os.path.join(DOFUS_PATH, "Dofus_Data", "StreamingAssets","Content", "Picto", "Items")}"
+    )
 
 
 def get_datas():
-    UABEA_PATH_EXE = os.path.join(
-        "D:\\",
-        "Workspace",
-        "UABEA",
-        "UABEAvalonia",
-        "bin",
-        "Debug",
-        "net6.0",
-        "UABEAvalonia.exe",
-    )
-
     os.system(
         f"{UABEA_PATH_EXE} batchexportbundle {os.path.join(DOFUS_PATH, "Dofus_Data", "StreamingAssets", "Content", "Map")}"
     )
@@ -36,6 +42,7 @@ def get_datas():
 
 
 if __name__ == "__main__":
+    # get_gfx()
     on_maj()
     get_datas()
     gen_all_python_class_datas()
