e85056fd3c1cc5615567ca681bfa4186f4e6a18d
Valentin Alix <valentin-alix.850@outlook.fr>
2024-11-23 16:45:47 +0100

    temp

--- src/generator/data_cleaning.py
+++ src/generator/data_cleaning.py
@@ -9,5 +9,6 @@ def clean_data_to_output(class_type: Any, filepath: str):
         content = msgspec.json.decode(file.read(), type=class_type)
 
     with open(filepath, "wb") as file:
-        file.write(zlib.compress(content, level=zlib.Z_BEST_COMPRESSION))
-        # file.write(msgspec.json.encode(content))
+        file.write(
+            zlib.compress(msgspec.json.encode(content), level=zlib.Z_BEST_COMPRESSION)
+        )
