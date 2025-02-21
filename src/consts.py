b997f85ab44f285888fdfc230cc054a2c03d4047
Valentin Alix <valentin-alix.850@outlook.fr>
2025-02-21 15:27:12 +0100

    remove proto mapping from repo

--- src/consts.py
+++ src/consts.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-from D3Database.consts import DOFUS_PATH
 
 UABEA_PATH_EXE = os.path.join(
     Path(__file__).parent.parent,
@@ -11,37 +10,4 @@ UABEA_PATH_EXE = os.path.join(
     "Debug",
     "net6.0",
     "UABEAvalonia.exe",
-)
-IL2_CPP_DUMPER_PATH_EXE = os.path.join(
-    Path(__file__).parent.parent,
-    "Il2CppDumper",
-    "Il2CppDumper",
-    "bin",
-    "Debug",
-    "net8.0",
-    "IL2CppDumper.exe",
-)
-PROTODEC_PATH_EXE = os.path.join(
-    Path(__file__).parent.parent,
-    "protodec",
-    "bin",
-    "protodec",
-    "Debug",
-    "net8.0",
-    "protodec.exe",
-)
-
-
-ASSEMBLIES_PATH = os.path.join(DOFUS_PATH, "assemblies")
-GAME_ASSEMBLY_PATH = os.path.join(DOFUS_PATH, "GameAssembly.dll")
-GLOBAL_METADATA_PATH = os.path.join(
-    DOFUS_PATH, "Dofus_Data", "il2cpp_data", "Metadata", "global-metadata.dat"
-)
-
-
-PROTO_CONNECTION_ASSEMBLY_PATH = os.path.join(
-    ASSEMBLIES_PATH, "DummyDll", "Ankama.Dofus.Protocol.Connection.dll"
-)
-PROTO_GAME_ASSEMBLY_PATH = os.path.join(
-    ASSEMBLIES_PATH, "DummyDll", "Ankama.Dofus.Protocol.Game.dll"
 )
--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,5 @@
-import importlib.util
 import re
-import sys
 
 
 def to_snake_case(value: str):
     return re.sub(r"(?<!^)(?=[A-Z])", "_", value).lower()
-
-
-def instantiate_class_from_path(class_name: str, file_path: str) -> type[object]:
-    spec = importlib.util.spec_from_file_location(class_name, file_path)
-    if spec is None:
-        raise FileNotFoundError
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[class_name] = module
-    spec.loader.exec_module(module)  # type: ignore
-
-    cls = getattr(module, class_name)
-
-    return cls
-
-
-if __name__ == "__main__":
-    _type = instantiate_class_from_path(
-        "Model",
-        "D:\\Workspace\\DB-DofusUnity\\db_dofus_unity\\models\\datas\\AbuseReasonsRoot.py",
-    )
-    print(_type)
